0db31e2e2a5fa21edde9d4a639d94c454dd30869
Nazire Celebi <nazcelebii@outlook.com>
2024-05-01 20:37:12 +0300

    finishh

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const curretYear = 2024;
+const currentYear = 2024;
 const weekDays = ['Pzt', 'Salı', 'Çrş', 'Prş', 'Cum', 'Cmrt', 'Pzr'];
 const months = [
     'Ocak',
@@ -14,16 +14,18 @@ const months = [
     'Kasım',
     'Aralık'
 ];
-
-const colors = ['#73a340', '#d4cb13', '#e05e08', '#e40404', '#003391'];
+const colors = ['#2d6b5f', '#72e3a6', '#dff4c7', '#edbf98', '#ea3d36'];
 const defaultColor = '#888';
 let activeColor = '';
 
 const calendar = document.getElementById('calendar');
 const moods = document.querySelectorAll('.mood');
+const randomize = document.querySelector('#randomize');
+const clear = document.querySelector('#clear');
 
 moods.forEach(mood => {
     mood.addEventListener('click', () => {
+
         if (mood.classList.contains('selected')) {
             mood.classList.remove('selected');
             activeColor = defaultColor;
@@ -38,21 +40,19 @@ moods.forEach(mood => {
     });
 });
 
-
-const getAllDay = year => {
-    //yılın ilk ve son günü belinlendi
-    const firstDay = new Date(`Ocak 1 $(year)`);
-    const lastDay = new Date(`Aralık 31 $(year)`);
+const getAllDays = year => {
+    const firstDay = new Date(`January 1 ${year}`);
+    const lastDay = new Date(`December 31 ${year}`);
 
     const days = [firstDay];
 
     let lastDayInArray = firstDay;
 
-    //bu döngü days dizisine yılın tüm günlerini ekleyecek
     while (lastDayInArray.getTime() !== lastDay.getTime()) {
         days.push(addDays(lastDayInArray, 1));
         lastDayInArray = days[days.length - 1];
     }
+
     return days;
 };
 
@@ -64,16 +64,18 @@ months.forEach((month, idx) => {
     monthsHTML += `<div class="months month_${idx}">
         <h3>${month}</h3>
         <div class="week_days_container">
-            ${weekDays.map(day => `<div class="week_days">${day}</div>`).join('')}</div>
+        ${weekDays.map(day => `<div class="week_days">${day}</div>`).join('')} </div>
         <div class="days_container"></div>
     </div>`;
 });
 
 calendar.innerHTML = monthsHTML;
 
+
 dates.forEach(date => {
     const month = date.getMonth();
     const monthEl = document.querySelector(`.month_${month} .days_container`);
+
 
     if (date.getDate() === 1 && date.getDay() !== 0) {
         for (let i = 0; i < date.getDay(); i++) {
@@ -82,10 +84,11 @@ dates.forEach(date => {
             monthEl.appendChild(emptySpot);
         }
     }
+
     const dateEl = createDateEl(date);
+
     monthEl.appendChild(dateEl);
 });
-
 
 const circles = document.querySelectorAll('.circle');
 circles.forEach(circle => {
@@ -93,7 +96,6 @@ circles.forEach(circle => {
         circle.style.backgroundColor = activeColor;
     });
 });
-
 
 function getRandomColor() {
     return colors[Math.floor(Math.random() * 5)];
@@ -104,18 +106,19 @@ function createDateEl(date) {
     const dateEl = document.createElement('div');
     dateEl.classList.add('days');
     dateEl.innerHTML = `<span class="circle">${day}</span>`;
+
     return dateEl;
 }
 
 function createEmptySpot() {
     const emptyEl = document.createElement('div');
     emptyEl.classList.add('days');
+
     return emptyEl;
 }
 
-
 function addDays(date, days) {
-	var result = new Date(date);
-	result.setDate(result.getDate() + days);
-	return result;
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
 }
